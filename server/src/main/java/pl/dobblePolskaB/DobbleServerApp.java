f4d049a4950a04a7e0f4fab79ef962d54aace5ec
Mateusz Wieczorek <mateusz.wieczorek@augmentersoft.com>
2019-12-03 20:55:36 +0100

    1. Configured Gradle project for server application. 2. Added main class for server application: DobbleServerApp.

---
+++ server/src/main/java/pl/dobblePolskaB/DobbleServerApp.java
@@ -0,0 +1,8 @@
+package pl.dobblePolskaB;
+
+public class DobbleServerApp {
+    public static void main(final String[] args) {
+        System.out.println("Hello World!");
+        return;
+    }
+}

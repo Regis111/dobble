f8b1c6c2b7d1eb8d1980392b4d9320c5087177a9
regis <jerzy.wenta@protonmail.com>
2019-12-04 00:32:29 +0100

    1. Configured gradle for spring boot application 2. Added hello world controller 3. Configured DobbleServerApp as main application, moved it to pl package as spring requires it there

--- server/src/main/java/pl/dobblePolskaB/DobbleServerApp.java
+++
@@ -1,8 +0,0 @@
-package pl.dobblePolskaB;
-
-public class DobbleServerApp {
-    public static void main(final String[] args) {
-        System.out.println("Hello World!");
-        return;
-    }
-}

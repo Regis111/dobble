377f7b0d18bb4d25aa11be4b897604f775457521
regis <jerzy.wenta@protonmail.com>
2019-12-04 16:39:50 +0100

    1. delete irrelevant main class 2. moved all files to dobblepolskab package

--- server/src/main/java/pl/dobblepolskab/DobbleServerApp.java
+++ server/src/main/java/pl/dobblepolskab/DobbleServerApp.java
@@ -1,8 +1,11 @@
 package pl.dobblepolskab;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class DobbleServerApp {
     public static void main(final String[] args) {
-        System.out.println("Hello World!");
-        return;
+        SpringApplication.run(DobbleServerApp.class, args);
     }
 }
--- server/src/main/java/pl/dobblepolskab/controller/MainController.java
+++ server/src/main/java/pl/dobblepolskab/controller/MainController.java
@@ -1,4 +1,4 @@
-package pl.controller;
+package pl.dobblepolskab.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;

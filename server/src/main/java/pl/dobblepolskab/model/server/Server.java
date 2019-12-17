133ef1fd4f66d860f64e9a682b7c6efc80de33c5
Mateusz Wieczorek <mateusz.wieczorek@augmentersoft.com>
2019-12-17 15:35:51 +0100

    Added method's prototypes for Server class. 1. Added method's prototypes for Server class necessary to code server-client part of system.

--- server/src/main/java/pl/dobblepolskab/model/server/Server.java
+++ server/src/main/java/pl/dobblepolskab/model/server/Server.java
@@ -1,5 +1,6 @@
 package pl.dobblepolskab.model.server;
 
+import com.sun.tools.javac.util.Pair;
 import pl.dobblepolskab.common.sockets.AssociationBasedServerSocket;
 import pl.dobblepolskab.common.sockets.ServerSocket;
 import pl.dobblepolskab.model.server.serverconfigurator.ServerConfigurator;
@@ -32,6 +33,46 @@ public class Server {
         serverSocket = new AssociationBasedServerSocket("dobbleServer");
         AIModule.setUpServerSocketInfo((AssociationBasedServerSocket)serverSocket);
         initiated = true;
+    }
+
+    public boolean setNumberOfPlayer(int value){
+        return false;
+    }
+
+    public boolean setComputerPlayersDifficultyLevel(int value){
+        return false;
+    }
+
+    public boolean addPlayerToGame(String playerName, String playerClientId){
+        return true;
+    }
+
+    public boolean deletePlayerFromGame(String playerName){
+        return false;
+    }
+
+    public boolean startGameSession(){
+        return true;
+    }
+
+    public boolean startNextShout(){
+        return true;
+    }
+
+    public int getCurrentShoutId(){
+        return 1;
+    }
+
+    public boolean isWinner(String playerClientId, int requestShoutId){
+        return true;
+    }
+
+    public Pair<Integer, Integer> getNextTurnState(String playerClientId){
+        return new Pair<>(0, 0);
+    }
+
+    public boolean endGameSession(){
+        return false;
     }
 
 
